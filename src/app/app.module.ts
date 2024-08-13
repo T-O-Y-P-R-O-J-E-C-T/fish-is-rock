59b8a1dffcf234cb73a79d164f3ba9504e199cae
박종국 <whdrnrdl789@naver.com>
2024-08-13 10:52:15 +0900

    feat: 로그인 초기 설정

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dbConfig } from '../../dbConfig';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(dbConfig)
+    TypeOrmModule.forRoot(dbConfig),
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const sessionMiddleware = session({
+    secret: process.env.secret,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 30 * 60 * 24,
+      httpOnly: true
+    }
+  });
+  app.use(sessionMiddleware);
   await app.listen(3000);
+
 }
 bootstrap();
