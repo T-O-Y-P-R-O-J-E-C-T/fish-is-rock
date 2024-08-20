1433742e124db0fcbd901886ca88e75220f90abc
박종국 <whdrnrdl789@naver.com>
2024-08-20 21:56:22 +0900

    feat: 스웨거 적용

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
-import session from 'express-session';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { setupSwagger } from './global/config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,8 @@ async function bootstrap() {
   //   }
   // });
   // app.use(sessionMiddleware);
+
+  setupSwagger(app);
 
   // 컨트롤러로 들어오는 페이로드를 해당 타입에 맞는 DTO 클래스로 변환
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
