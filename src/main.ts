206f4d8f7cee6c0d0a4d194859db7f026492174c
ASTER18K <donhon05@gmail.com>
2024-09-05 00:43:19 +0900

    FIX: Add comments to main.ts and modify the package management file

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { setupSwagger } from './global/config/swagger.config';
+// import { JwtRefreshTokenGuard } from './auth/guard/refreshToken.guard';
+// import { JwtAccessTokenGuard } from './auth/guard/accessToken.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   setupSwagger(app);
   // 컨트롤러로 들어오는 페이로드를 해당 타입에 맞는 DTO 클래스로 변환
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  // app.useGlobalGuards(new JwtAccessTokenGuard());
+  // app.useGlobalGuards(new JwtRefreshTokenGuard());
   await app.listen(3000);
 }
 bootstrap();
